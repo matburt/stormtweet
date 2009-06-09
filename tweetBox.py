642ca3ed3c17748fcbaae32105de0fe1add1a451
tiberius <tiberius@macross.7st.homelinux.org>
2009-06-08 21:43:33 -0500

    made two wrapper functions for twitter

--- tweetBox.py
+++ tweetBox.py
@@ -1,5 +1,16 @@
 from twitter import Twitter
 
-tbox=Twitter('stormwarn','st0rmp22wd')
+##tbox=Twitter('stormwarn','st0rmp22wd')
 
-tbox.statuses.update(status='testing python call')
+##tbox.statuses.update(status='testing python call')
+
+def stormMessage(message, receipients):
+    tbox=Twitter('stormwarn','st0rmp22wd')
+##    tbox.direct_messages.new(user='timothy_edwards',message)
+    for aUser in receipients:
+        tbox.direct_messages.new(user=aUser,message)
+
+
+def update(message):
+    tbox=Twitter('stormwarn','st0rmp22wd')
+    tbox.statuses.update(status='message')
