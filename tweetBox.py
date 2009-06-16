d7f20a687a50150d82fc8562f8a14d0587d039b0
User <mat@matburt.net>
2009-06-16 04:07:05 +0000

    Small bugfixes

--- tweetBox.py
+++ tweetBox.py
@@ -1,16 +1,13 @@
 from twitter import Twitter
 from model import *
-xs
 
 class TweetBox:
     def __init__(self, user, password):
         self.tbox=Twitter(user, password)
 
     def stormMessage(message, receipients):
-        tbox=Twitter('stormwarn','st0rmp22wd')
         for aUser in receipients:
-            tbox.direct_messages.new(user=aUser,text=message)
+            self.tbox.direct_messages.new(user=aUser,text=message)
 
     def update(message):
-        tbox=Twitter('stormwarn','st0rmp22wd')
-        tbox.statuses.update(status='message')
+        self.tbox.statuses.update(status='message')
